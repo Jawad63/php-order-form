ba739dd6d25bb0f9e4bdd90e4b0873564d2c54eb
Jawad63 <jawadqureshi111@gmail.com>
2021-08-03 02:13:50 +0200

    order-form complete

--- form-view.php
+++ form-view.php
@@ -1,5 +1,5 @@
-
-<?php // This files is mostly containing things for your view / html ?>
+<?php // This files is mostly containing things for your view / html
+?>
 
 <!doctype html>
 <html lang="en">
@@ -10,115 +10,100 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" type="text/css"
           rel="stylesheet"/>
-    <title>Your fancy store</title>
-    <link rel="stylesheet" href="style.css">
+    <title>Console Mania</title>
+    <link rel="stylesheet" href="./style.css">
+
 </head>
 <body>
 <div class="container">
+
     <h1>Place your order</h1>
-    <?php if (!empty($result['message'])) { ?>
-        <div class="alert <?php if ($result['errors']) { echo 'alert-danger'; } else { echo 'alert-success'; } ?>">
-            <?= $result['message'] ?>
-        </div>
-    <?php }; ?>
 
-    <!--
-    <nav>
-        <ul class="nav">
-            <li class="nav-item">
-                <a class="nav-link active" href="?food=1">Order playstation 5</a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link" href="?food=0">Order Xbox X</a>
-            </li>
-        </ul>
-    </nav>
-    -->
-
-    <div class="container">
-        <div class="row">
-            <div class="col-md-4 product">
-                <div class="card" style="width: 18rem;">
-                    <img src="./IMG/playstation.jpg" class="card-img-top" alt="playstation">
-                    <div class="card-body">
-                        <h5 class="card-title">Playstation 5</h5>
-                        <p class="card-text">Get your hands on the new PS5 :)</p>
-                        <a href="#" class="btn btn-primary">Order now!</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4 product">
-                <div class="card" style="width: 18rem;">
-                    <img src="./IMG/xbox.jpg" class="card-img-top" alt="xbox">
-                    <div class="card-body">
-                        <h5 class="card-title">Xbox X</h5>
-                        <p class="card-text">Or just buy this :/</p>
-                        <a href="#" class="btn btn-primary">Order?</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    
-
-
-    <form method="post" >
+    <form method="post">
 
         <div class="form-row">
             <div class="form-group col-md-6">
-                <label for="email">E-mail:</label>
-                <input type="email" id="email" name="email" class="form-control"/>
-                <span class="error">*<?php echo $emailErr;?></span>
+                <label class="heading" for="email">E-mail:</label>
+                <input type="email" id="email" name="email" class="form-control"
+                       value="<?php echo isset($_POST["email"]) ? $_POST["email"] : ''; ?>">
             </div>
+            <div></div>
         </div>
 
         <fieldset>
-            <legend>Address</legend>
+            <legend class="legend">Address</legend>
 
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="street">Street:</label>
-                    <input type="text" name="street" id="street" class="form-control">
+                    <label class="heading" for="street">Street:</label>
+                    <input type="text" name="street" id="street" class="form-control"
+                           value="<?php echo isset($_POST["street"]) ? $_POST["street"] : ''; ?>">
                 </div>
-
                 <div class="form-group col-md-6">
-                    <label for="streetnumber">Street number:</label>
-                    <input type="text" id="streetnumber" name="streetnumber" class="form-control">
+                    <label class="heading" for="streetnumber">Street number:</label>
+                    <input type="text" id="streetnumber" name="streetnumber" class="form-control"
+                           value="<?php echo isset($_POST["streetnumber"]) ? $_POST["streetnumber"] : ''; ?>">
                 </div>
             </div>
 
-
             <div class="form-row">
                 <div class="form-group col-md-6">
-                    <label for="city">City:</label>
-                    <input type="text" id="city" name="city" class="form-control">
+                    <label class="heading" for="city">City:</label>
+                    <input type="text" id="city" name="city" class="form-control"
+                           value="<?php echo isset($_POST["city"]) ? $_POST["city"] : ''; ?>">
                 </div>
-
                 <div class="form-group col-md-6">
-                    <label for="zipcode">Zipcode</label>
-                    <input type="text" id="zipcode" name="zipcode" class="form-control">
+                    <label class="heading" for="zipcode">Zipcode</label>
+                    <input type="text" id="zipcode" name="zipcode" class="form-control"
+                           value="<?php echo isset($_POST["zipcode"]) ? $_POST["zipcode"] : ''; ?>">
                 </div>
             </div>
         </fieldset>
+
+        
 
         <fieldset>
             <legend>Products</legend>
-
             <?php foreach ($products as $i => $product): ?>
                 <label>
-					<?php // <?p= is equal to <?php echo ?>
-                    <input type="checkbox" value="1" name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?> -
-                    &euro; <?= number_format($product['price'], 2) ?></label><br />
+                    <?php // <?p= is equal to <?php echo ?>
+                    <input type="checkbox" value="1" <?php if (isset($_POST['products'][$i])) {
+                        foreach ($_POST['products'] as $tmp) {
+                            if ($tmp == "1") {
+                                echo "checked='checked'";
+                            }
+                        }
+                    } ?> name="products[<?php echo $i ?>]"/> <?php echo $product['name'] ?>
+                    -
+                    &euro; <?= number_format($product['price'], 2) ?></label><br/>
             <?php endforeach; ?>
-            
         </fieldset>
 
-        <button type="submit" class="btn btn-primary">Order!</button>
+
+
+        <button type="submit" class="btn btn-primary">Order Now!</button>
     </form>
 
+    <br><br><br>
 
 
-    <footer>You already ordered <strong>&euro; <?php echo $totalValue ?></strong> worth of electronics!</footer>
+    <!-- display message that the purchase is successful-->
+    <?php if (!empty($result['message'])) { ?>
+
+    <div class="alert <?php if ($result['errors']) {
+        echo 'alert-danger';
+    }
+    else {
+        echo 'alert-success';
+    } ?>">
+        <?= $result['message'] ?>
+    </div>
+
+    <?php }; ?>
+
+
+
+    <footer><strong>your total is: &euro; <?php echo $totalValue ?></strong></footer>
 </div>
 
 </body>
--- index.php
+++ index.php
@@ -1,28 +1,113 @@
 <?php
-// setting variable here:
-$emailErr = "";
 
-// making the condition:
-if (empty($_POST["email"])) {
-    $emailErr = "Email is required";
-  }
-  
-else {
-    $email = test_input($_POST["email"]);
+   // This line makes PHP behave in a more strict way
+      declare(strict_types=1);
+      ini_set('display_errors', '1');
+      ini_set('display_startup_errors', '1');
+      error_reporting(E_ALL);
+   // We are going to use session variables so we need to enable sessions
+      session_start();
 
-    // check if e-mail address is well-formed:
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-      $emailErr = "Invalid email format";
-    }
-}
+   // Use this function when you need to need an overview of these variables
+      function whatIsHappening() {
+      echo '<pre>';
+      echo '<h2>$_GET</h2>';
+      var_dump($_GET);
+      echo '<h2>$_POST</h2>';
+      var_dump($_POST);
+      echo '<h2>$_COOKIE</h2>';
+      var_dump($_COOKIE);
+      echo '<h2>$_SESSION</h2>';
+      var_dump($_SESSION);
+      echo '</pre>';
+   }
 
-// don't know what this is? 
-function test_input($data) {
-  $data = trim($data);
-  $data = stripslashes($data);
-  $data = htmlspecialchars($data);
-  return $data;
-}
+   //whatIsHappening();
+   $products = [
+      ['name' => 'Playstation 5', 'price' => 499],
+      ['name' => 'Xbox Series X', 'price' => 499],
+      ['name' => 'Playstation 4 pro', 'price' => 349],
+      ['name' => 'Xbox Series S', 'price' => 299]
+   ];
 
 
-?>
+   $totalValue = 0;
+
+   function validate() {
+
+      // This function will send a list of invalid fields back
+      $errors = [];
+
+      if (empty($_POST['email'])) {
+         $errors[] = 'email';
+      }
+      if (empty($_POST['street'])) {
+         $errors[] = 'street';
+      }
+      if (empty($_POST['streetnumber'])) {
+         $errors[] = 'streetnumber';
+      }
+      if (empty($_POST['city'])) {
+         $errors[] = 'city';
+      }
+      if (empty($_POST['zipcode']) or !is_numeric($_POST['zipcode'])) {
+         $errors[] = 'zipcode';
+      }
+      return $errors;
+   }
+
+
+   function handleForm($products, &$totalValue) {
+   //     & changes the original value
+
+   // Validation (step 2)
+   $invalidFields = validate();
+   if (!empty($invalidFields)) {
+      $message = '';
+      foreach ($invalidFields as $invalidField) {
+         $message .= "Please provide your {$invalidField}.";
+         $message .= '<br>';
+      }
+      return [
+         'errors' => true,
+         'message' => $message
+      ];
+   } 
+   
+   else {
+
+        $productNumbers = array_keys($_POST['products']);
+        $productNames = [];
+
+        foreach ($productNumbers as $productNumber) {
+            $productNames[] = $products[$productNumber]['name'];
+            $totalValue = $totalValue + $products[$productNumber]['price'];
+         }
+
+        $message = 'Products : ' . implode(', ', $productNames);
+        $message .= '<br>';
+        $message .= 'Your email address : ' . $_POST['email'];
+        $_SESSION['email'] = $_POST['email'];
+        $message .= '<br>';
+        $message .= 'Your address : ' . $_POST['street'] . ' ' . $_POST['streetnumber'] . ', ' . $_POST['zipcode'] . ' ' . $_POST['city'];
+        $_SESSION['street'] = $_POST['street'];
+        $_SESSION['streetnumber'] = $_POST['streetnumber'];
+        $_SESSION['zipcode'] = $_POST['zipcode'];
+        $_SESSION['city'] = $_POST['city'];
+
+         return [
+            'errors' => false,
+            'message' => $message,
+            'totalValue' => $totalValue
+         ];
+      }
+   }
+
+   $formSubmitted = !empty($_POST);
+   $result = [];
+
+   if ($formSubmitted) {
+      $result = handleForm($products, $totalValue);
+   }
+
+   require 'form-view.php';
